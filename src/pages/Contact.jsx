a620e2e44918144645211acf3e2961b9401ac537
Muhammad-Shariq365 <108149127+Muhammad-Shariq365@users.noreply.github.com>
2024-03-07 01:20:44 +0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -120,7 +120,7 @@ const Contact = () => {
 
           <button
             type='submit'
-            className='btn'
+            className='btn bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
             disabled={isLoading}
             onFocus={handleFocus}
             onBlur={handleBlur}
